c163090d09b043837cdb080f48f0cddce7d6291a
Jeremiah Ang <s43500@sp.edu.sg>
2022-01-18 17:38:39 +0800

    Add skeleton

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,8 @@
+let isInitialFetched = false;
 document.addEventListener('DOMContentLoaded', function () {
     const tweetsDom = document.querySelector('#tweets');
     const tweetTemplate = document.querySelector('#tweet-template').content.firstElementChild;
+    const loadingSkeleton = document.querySelector('#skeleton');
     function addTweet(tweet) {
         const tweetDom = tweetTemplate.cloneNode(true);
         tweetDom.querySelector('.tweet-name').textContent = tweet.name;
@@ -10,7 +12,25 @@ document.addEventListener('DOMContentLoaded', function () {
         tweetDom.querySelector('.tweet-img').src = tweet.img;
         tweetsDom.appendChild(tweetDom);
     }
+
     fetch('/data')
         .then((response) => response.json())
-        .then((json) => json.data.forEach(addTweet));
+        .then((json) => json.data.forEach(addTweet))
+        .finally(() => {
+            isInitialFetched = true;
+            loadingSkeleton.classList.remove('loading');
+        });
+
+    let isFetching = false;
+    window.onscroll = function () {
+        if (isInitialFetched && Math.ceil(window.innerHeight + window.pageYOffset) >= document.body.scrollHeight) {
+            if (isFetching) return;
+            isFetching = true;
+            loadingSkeleton.classList.add('loading');
+            // Replace the following 3 lines
+            setTimeout(() => {
+                loadingSkeleton.classList.remove('loading');
+            }, 2000);
+        }
+    };
 });
